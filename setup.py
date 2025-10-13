b23a22de6f28f6f1dddab4eff5fc78f7968a8c47
Brett Brillhart <bbrillhart19@gmail.com>
2025-10-12 22:03:58 -0400

    Bump version v0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="vapor",
-    version="0.0.2",
+    version="0.0.3",
     description="Personalized AI Companion for Steam",
     author="Brett Brillhart",
     author_email="bbrillhart19@gmail.com",
