48577b4db36f3b2c13e47363d36653a436cce6d3
Brett Brillhart <bbrillhart19@gmail.com>
2025-10-30 15:33:15 -0400

    Remove unnecessary utility methods

--- vapor/utils/utils.py
+++ vapor/utils/utils.py
@@ -1,20 +1,6 @@
+"""Miscellaneous utility methods"""
+
 import os
-from pathlib import Path
-import shutil
-
-
-def cast_path(pth: Path | str) -> Path:
-    if isinstance(pth, str):
-        return Path(pth)
-    return pth
-
-
-def create_dir(pth: Path | str, clear_contents: bool = False) -> Path:
-    pth = cast_path(pth)
-    if pth.exists() and clear_contents:
-        shutil.rmtree(pth)
-    pth.mkdir(exist_ok=True, parents=True)
-    return pth
 
 
 def get_env_var(env_key: str, non_env_val: str | None = None) -> str:
