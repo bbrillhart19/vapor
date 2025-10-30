ca8f3b501a59dcab3bae70feb281e5e977e771bc
Brett Brillhart <bbrillhart19@gmail.com>
2025-10-30 15:33:41 -0400

    Removing data path construction

--- vapor/__init__.py
+++ vapor/__init__.py
@@ -3,10 +3,5 @@ from dotenv import load_dotenv
 from .utils import utils
 
 # Set up environment file and load
-ENV_FILE = utils.cast_path(utils.get_env_var("VAPOR_ENV", "./.env"))
-if not ENV_FILE.exists():
-    raise FileNotFoundError(f"Could not load environment from file={ENV_FILE}")
+ENV_FILE = utils.get_env_var("VAPOR_ENV", "./.env")
 load_dotenv(ENV_FILE)
-
-# Set up path for data
-DATA_PATH = utils.cast_path(utils.get_env_var("VAPOR_DATA_PATH"))
