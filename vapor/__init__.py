992019625e94644f0e55d0ce96ad384484a0cd94
Brett Brillhart <bbrillhart19@gmail.com>
2025-10-30 14:36:15 -0400

    Remove logging configuration

--- vapor/__init__.py
+++ vapor/__init__.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 
-from .utils import utils, logging
+from .utils import utils
 
 # Set up environment file and load
 ENV_FILE = utils.cast_path(utils.get_env_var("VAPOR_ENV", "./.env"))
@@ -10,7 +10,3 @@ load_dotenv(ENV_FILE)
 
 # Set up path for data
 DATA_PATH = utils.cast_path(utils.get_env_var("VAPOR_DATA_PATH"))
-
-# Initialize the log file/logger instance
-# TODO: Keep?
-# logging.init_logger()
