9854dbe31e4a98697d227f5ce32f19a685abb801
Brett Brillhart <bbrillhart19@gmail.com>
2025-10-31 11:38:27 -0400

    Complete unit tests for steam2neo4j population methods [ref #7]

---
+++ tests/test_steam2neo4j.py
@@ -0,0 +1,170 @@
+import os
+
+from vapor.clients import SteamClient, Neo4jClient
+from vapor.utils import steam2neo4j
+from helpers import globals
+
+
+def test_populate_friends(
+    mocker,
+    steam_client: SteamClient,
+    neo4j_client: Neo4jClient,
+    steam_friends: dict[str, list[dict]],
+):
+    """Tests populating friends from Steam to Neo4j"""
+    # First set up neo4j and primary user
+    neo4j_client.setup_from_primary_user(steamid=globals.STEAM_ID, personaname="user0")
+
+    # Mock the steam client to return friends per user
+    def mocked_friends(steamid: str, *args, **kwargs):
+        friends = steam_friends[steamid]
+        if "limit" in kwargs:
+            friends = friends[: kwargs["limit"]]
+        for friend in friends:
+            yield friend
+
+    mocker.patch.object(
+        SteamClient,
+        "get_user_friends",
+        side_effect=mocked_friends,
+    )
+    # Run the friends population method w/ a small limit to ensure brevity
+    limit = 2
+    steam2neo4j.populate_friends(
+        steam_client, neo4j_client, steamid=None, hops=2, limit=limit
+    )
+    # Verify expected friendships (first hop=primary user)
+    cypher = """
+        MATCH (p:Primary)-[:HAS_FRIEND]->(u:User)
+        RETURN u.steamId as steamid
+    """
+    result = neo4j_client._read(cypher)
+    result_steamids = set(result["steamid"])
+    expected_steamids = set(
+        x["steamid"] for x in steam_friends[globals.STEAM_ID][:limit]
+    )
+    assert not expected_steamids - result_steamids
+    # Verify second hop friendships
+    cypher = """
+        MATCH (u:User {steamId: $steamid})-[:HAS_FRIEND]->(f:User)
+        RETURN f.steamId as steamid
+    """
+    for steamid in expected_steamids:
+        result = neo4j_client._read(cypher, steamid=steamid)
+        result_steamids = set(result["steamid"])
+        _expected_steamids = set(x["steamid"] for x in steam_friends[steamid][:limit])
+        assert not _expected_steamids - result_steamids
+
+
+def test_populate_games(
+    mocker,
+    steam_client: SteamClient,
+    neo4j_client: Neo4jClient,
+    steam_owned_games: dict[str, list[dict]],
+):
+    """Tests populating user-owned games from Steam to Neo4j"""
+    # Add some users to populate games from
+    limit = 2
+    steamids = list(steam_owned_games.keys())[:limit]
+    cypher = """
+        UNWIND $steamids as steamid
+        MERGE (u:User {steamId: steamid})
+    """
+    neo4j_client._write(cypher, steamids=steamids)
+
+    # Mock the steam client to return owned and
+    # recently played games for each user
+    def mocked_owned_games(steamid: str, *args, **kwargs):
+        owned_games = steam_owned_games[steamid]
+        if "limit" in kwargs:
+            owned_games = owned_games[: kwargs["limit"]]
+        for game in owned_games:
+            yield game
+
+    mocker.patch.object(
+        SteamClient,
+        "get_user_owned_games",
+        side_effect=mocked_owned_games,
+    )
+    # Just use the same mocked function for recently played
+    mocker.patch.object(
+        SteamClient,
+        "get_user_recently_played_games",
+        side_effect=mocked_owned_games,
+    )
+    # Run the games population method
+    steam2neo4j.populate_games(steam_client, neo4j_client, limit=limit)
+    # Verify the expected games relationships
+    owned_games_cypher = """
+        MATCH (u:User {steamId: $steamid})-[r:OWNS_GAME]->(g:Game)
+        RETURN g.appId as appid, g.name as name, r.playtime as playtime_forever
+    """
+    recently_played_cypher = """
+        MATCH (u:User {steamId: $steamid})-[r:RECENTLY_PLAYED]->(g:Game)
+        RETURN g.appId as appid, g.name as name, r.recentPlaytime as playtime_2weeks
+    """
+    for steamid in steamids:
+        expected_games = steam_owned_games[steamid][:limit]
+        owned_result = neo4j_client._read(owned_games_cypher, steamid=steamid)
+        recently_played_result = neo4j_client._read(
+            recently_played_cypher, steamid=steamid
+        )
+        for game in expected_games:
+            assert not owned_result.loc[
+                (owned_result["appid"] == game["appid"])
+                & (owned_result["name"] == game["name"])
+                & (owned_result["playtime_forever"] == game["playtime_forever"])
+            ].empty
+            assert not recently_played_result.loc[
+                (recently_played_result["appid"] == game["appid"])
+                & (recently_played_result["name"] == game["name"])
+                & (recently_played_result["playtime_2weeks"] == game["playtime_2weeks"])
+            ].empty
+
+
+def test_populate_genres(
+    mocker,
+    steam_client: SteamClient,
+    neo4j_client: Neo4jClient,
+    steam_games: dict[int, dict],
+):
+    """Tests populating game genres from Steam to Neo4j"""
+    # Add some games to populate genres from
+    limit = 2
+    games = list(steam_games.values())[:limit]
+    cypher = """
+        UNWIND $games as game
+        MERGE (g:Game {appId: game.appid})
+    """
+    neo4j_client._write(cypher, games=games)
+
+    # Mock the steam client to return genres for each game
+    def mocked_game_details(appid: int, *args, **kwargs):
+        # Test no genres for the first game
+        if appid == games[0]["appid"]:
+            return {}
+        return {"genres": steam_games[appid]["genres"]}
+
+    mocker.patch.object(
+        SteamClient,
+        "get_game_details",
+        side_effect=mocked_game_details,
+    )
+    # Run the genres population method
+    steam2neo4j.populate_genres(steam_client, neo4j_client)
+    # Verify the expected genres
+    cypher = """
+        MATCH (g:Game {appId: $appid})-[:HAS_GENRE]->(n:Genre)
+        RETURN n.genreId as id, n.description as description
+    """
+    for i, game in enumerate(games):
+        result = neo4j_client._read(cypher, appid=game["appid"])
+        # First game should not exists
+        if i == 0:
+            assert result.empty
+            continue
+        for expected_genre in game["genres"]:
+            assert not result.loc[
+                (result["id"] == expected_genre["id"])
+                & (result["description"] == expected_genre["description"])
+            ].empty
---
+++ vapor/populate.py
@@ -0,0 +1,120 @@
+from loguru import logger
+
+from vapor import clients
+from vapor.utils import steam2neo4j
+
+
+@logger.catch
+def populate_neo4j(
+    hops: int = 2,
+    init: bool = False,
+    delete: bool = False,
+    friends: bool = False,
+    games: bool = False,
+    genres: bool = False,
+    limit: int | None = None,
+) -> None:
+    """Entry point to populate data. Initializes steam/neo4j from env vars."""
+    logger.info("Initializing SteamClient...")
+    steam_client = clients.SteamClient.from_env()
+    logger.info("Initializing Neo4jClient...")
+    neo4j_client = clients.Neo4jClient.from_env()
+
+    # Setup neo4j with primary user
+    if init:
+        logger.info("Retrieving primary user details and setting up...")
+        primary_user = steam_client.get_primary_user_details(["steamid", "personaname"])
+        neo4j_client.setup_from_primary_user(**primary_user)
+
+    # Clear everything and return
+    if delete:
+        neo4j_client.clear()
+        return
+
+    # Check for setup before proceeding with anything else
+    assert neo4j_client.is_setup, (
+        f"Neo4j is not setup properly and thus cannot be populated. "
+        + "Make sure you run `populate_neo4j` with `init` enabled."
+    )
+
+    # Populate friends spanning from primary user
+    if friends:
+        logger.info("Populating Steam users from friends lists...")
+        steam2neo4j.populate_friends(
+            steam_client, neo4j_client, steamid=None, hops=hops, limit=limit
+        )
+
+    # Populate games via all users (primary and friends)
+    if games:
+        logger.info("Populating Steam games from available Steam users...")
+        steam2neo4j.populate_games(steam_client, neo4j_client, limit=limit)
+
+    # Populate genres via all games
+    if genres:
+        logger.info("Populating genees from available Steam games...")
+        steam2neo4j.populate_genres(steam_client, neo4j_client)
+
+    logger.success("Completed Neo4j population sequence >>>")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Set up and populate neo4j database with steam data for Vapor"
+    )
+    parser.add_argument(
+        "-n",
+        "--hops",
+        type=int,
+        help="Number of hops to populate with. Defaults to 2.",
+        default=2,
+    )
+    parser.add_argument(
+        "-i",
+        "--init",
+        action="store_true",
+        help="Initialize the neo4j database with necessary constraints"
+        + " and set up the primary user. Disabled by default.",
+    )
+    parser.add_argument(
+        "-D",
+        "--delete",
+        action="store_true",
+        help="WARNING: This will delete all nodes and relationships in the graph."
+        + " Disabled by default.",
+    )
+    parser.add_argument(
+        "-f",
+        "--friends",
+        action="store_true",
+        help="Populate friends starting from primary spanning out `n_hops`."
+        + " Requires prior initialized neo4j database. Disabled by default.",
+    )
+    parser.add_argument(
+        "-g",
+        "--games",
+        action="store_true",
+        help="Populate games via users starting from primary spanning out `n_hops`."
+        + " Requires prior initialized neo4j database with friends."
+        + " Disabled by default.",
+    )
+    parser.add_argument(
+        "-G",
+        "--genres",
+        action="store_true",
+        help="Populate all game genres for all games present in the database."
+        + " Requires prior initialized neo4j database with games."
+        + " Disabled by default.",
+    )
+    parser.add_argument(
+        "-l",
+        "--limit",
+        type=int,
+        help="Limits all populating queries (friends, games, etc.) to this value."
+        + " If None, all discovered datums will be included. Defaults to None.",
+        default=None,
+    )
+
+    args = parser.parse_args()
+    populate_neo4j(**args.__dict__)
