dbc3611d805712abc9b185b973fc3c5a8a5261b0
Brett Brillhart <bbrillhart19@gmail.com>
2025-11-10 12:23:50 -0500

    Update dev-setup script with game-description embeddings

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -3,6 +3,7 @@
 # Set up dev neo4j instance
 docker compose -f compose.dev.yaml up -d &&
 # Populate it
-python vapor/populate.py -i -f -g -G -t -l 3 --dev
+python vapor/populate.py -i -f -g -G -d -l 3 \
+    --embed game-descriptions --dev
 
 echo "Dev setup complete >>>"
