33403be74c6af487f13addb563aaf6a3463cfd3c
Brett Brillhart <bbrillhart19@gmail.com>
2025-11-10 12:24:38 -0500

    Tweak comments in run-tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,9 +3,8 @@
 test_args=$1
 # Always teardown neo4j-dev first just in case it's running
 docker compose -f compose.dev.yaml down
-# Spin up neo4j and use helper script to wait for connection
+# Spin up neo4j
 docker compose -f compose.dev.yaml up -d && 
-python tests/helpers/verify_neo4j_connect.py &&
 # Run tests
 pytest --cov=vapor $test_args 
 # Teardown neo4j
