b496b966dee8fa8d57796948671783142d5a9753
Brett Brillhart <bbrillhart19@gmail.com>
2026-01-02 16:06:52 -0500

    Create mcp tools for game with entry point to run server [ref #13]

--- vapor/svc/mcp/__init__.py
+++ vapor/svc/mcp/__init__.py
@@ -1,8 +1,1 @@
-from . import games
-
-GAME_TOOLS = [
-    games.about_the_game,
-    games.find_similar_games,
-]
-
-ALL_TOOLS = GAME_TOOLS + []
+from .games import GamesTools
---
+++ vapor/svc/mcp/run.py
@@ -0,0 +1,24 @@
+from loguru import logger
+from fastmcp import FastMCP
+
+from vapor.core.clients import Neo4jClient
+from vapor.core.models.embeddings import VaporEmbeddings
+from vapor.svc.mcp import GamesTools
+
+logger.info("Initializing Vapor MCP Server >>>")
+mcp = FastMCP("Vapor MCP Server")
+
+logger.info("Setting up Neo4JClient...")
+neo4j_client = Neo4jClient.from_env()
+logger.info("Setting up Embedding Model...")
+embedder = VaporEmbeddings.from_env()
+
+logger.info("Setting up Games Tools...")
+games = GamesTools(
+    mcp_instance=mcp,
+    neo4j_client=neo4j_client,
+    embedder=embedder,
+)
+
+if __name__ == "__main__":
+    mcp.run()
