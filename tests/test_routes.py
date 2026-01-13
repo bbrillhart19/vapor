579679ebbab768db65959b620f043e0d689f2f4c
Brett Brillhart <bbrillhart19@gmail.com>
2026-01-12 22:39:18 -0500

    Final tests for create app and health check

---
+++ tests/test_routes.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from httpx import AsyncClient, ASGITransport
+
+from vapor.svc import routes
+
+
+### Status routes ###
+async def test_health_check():
+    """Tests the /status/health endpoint"""
+    app = FastAPI()
+    app.include_router(routes.status_router)
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test",
+    ) as client:
+        response = await client.get("/status/health")
+
+    assert response.json() == {"status": "alive"}
+    assert response.status_code == 200
