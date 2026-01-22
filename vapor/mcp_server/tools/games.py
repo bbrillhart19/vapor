bbe78a2ac35b783e5d4f589c0ce36203a601a2ae
Brett Brillhart <bbrillhart19@gmail.com>
2026-01-22 10:45:01 -0500

    Tools refactored to return response schemas [ref #15]

--- vapor/mcp_server/tools/games.py
+++ vapor/mcp_server/tools/games.py
@@ -4,6 +4,11 @@ from fastmcp.dependencies import Depends
 from vapor.core.dao import GamesDAO
 from vapor.core.dao.deps import get_games_dao
 from vapor.core.models.embeddings import VaporEmbeddings, get_embedder
+from vapor.mcp_server.schemas import (
+    AboutTheGameResponse,
+    FindSimilarGamesResponse,
+    SimilarGame,
+)
 
 
 class GamesTools(object):
@@ -16,7 +21,7 @@ class GamesTools(object):
 
     async def about_the_game(
         self, name: str, dao: GamesDAO = Depends(get_games_dao)
-    ) -> dict[str, str]:
+    ) -> AboutTheGameResponse:
         """Retrieves the "about the game" description for the game
         in the database that best matches the provided `name` using
         a fuzzy match technique. The game descriptions have been populated
@@ -29,22 +34,22 @@ class GamesTools(object):
                 match to the stored game titles.
 
         Returns:
-            dict[str, str]: Dictionary with fields describing the game
-                with the title that best matched the query name. The "matched_game"
-                field will provide the name/title of the game that best matched the
-                query name, or will not be present if no match was found.
-                The "about_the_game" field will provide the game description
-                for the best matched game, or will not be present
-                if no pre-populated description is available.
+            AboutTheGameResponse: Response containing the matched game name
+                and its description. Fields will be None if no match was found
+                or if the matched game has no description available.
         """
-        return dao.about_the_game(name)
+        result = dao.about_the_game(name)
+        return AboutTheGameResponse(
+            matched_game=result.get("matched_game"),
+            about_the_game=result.get("about_the_game"),
+        )
 
     async def find_similar_games(
         self,
         summarized_description: str,
         dao: GamesDAO = Depends(get_games_dao),
         embedder: VaporEmbeddings = Depends(get_embedder),
-    ) -> list[dict]:
+    ) -> FindSimilarGamesResponse:
         """Finds games and excerpts of their "about the game" descriptions
         in the database which are semantically similar to the
         provided `summarized_description`. Provides the discovered games
@@ -58,21 +63,19 @@ class GamesTools(object):
                 for semantic similarity search in the vector database.
 
         Returns:
-            list[dict]: List of dictionaries for each discovered similar
-                game as well as the description chunks most similar
-                to the `summarized_description`. Each discovered similar game
-                will include a "name" field with the title of the game,
-                a "appid" field with the unique Steam game ID of the game,
-                and a "description_chunks" field which will contain a list
-                of each similar description excerpt from the game.
-                If no similar games are found, the returned response
-                will be an empty list.
+            FindSimilarGamesResponse: Response containing a list of similar games,
+                each with the game name, Steam app ID, and description chunks
+                that were semantically similar to the query. The list will be
+                empty if no similar games were found.
         """
         # Create an embedding of the summarized description
         embedding = embedder.embed_query(summarized_description)
         # Use DAO to perform semantic search
-        return dao.find_similar_games(
+        results = dao.find_similar_games(
             embedding=embedding,
             n_neighbors=10,
             min_score=0.5,
+        )
+        return FindSimilarGamesResponse(
+            similar_games=[SimilarGame(**game) for game in results]
         )
